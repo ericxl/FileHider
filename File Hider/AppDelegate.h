aeb6fa64fb0472fb4ea65c0d4995385065949099
Eric Liang <Eric@ftp.apprilo.com>
2013-06-08 23:22:29 +0800

    Initial Commit

---
+++ File Hider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  File Hider
+//
+//  Created by Eric Liang on 6/8/13.
+//  Copyright (c) 2013 Eric Liang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ File Hider/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  File Hider
+//
+//  Created by Eric Liang on 6/8/13.
+//  Copyright (c) 2013 Eric Liang. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
